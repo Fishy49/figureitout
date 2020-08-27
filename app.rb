a78692d1fff0a23d68632b72f0b844ea741077f2
Richard Cagle <Rick.Cagle@Fabricut.com>
2020-08-27 00:25:21 -0500

    add variable intro sentences

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require "sinatra"
 require "titleize"
 
+INTRO_SENTENCES = [
+  "There's really only one solution to this:",
+  "You can approach this a number of ways but the best way is:",
+  "I can't stress this enough. Rule #1 for problems like this:",
+  "Luckily the solution is pretty straightforward. Just do the following:",
+  "My time-honored approach to this problem can be found below:"
+]
+
 get "/" do
-  erb :page, locals: { title: "How Did I Solve All My Problems?" }
+  erb :page, locals: { title: "How Did I Solve All My Problems?", intro: INTRO_SENTENCES.sample }
 end
 
 get "/:title" do
