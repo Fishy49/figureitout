cca912a5a225e275e39580e071bdb2c2d23c2dbe
Todd Yandell <tyandell@gmail.com>
2020-08-28 18:21:19 -0500

    Seed random

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-require 'active_support/inflector'
-require "faker"
-require "sinatra"
-require "titleize"
+require "bundler/setup"
+
+Bundler.require
+
+require "sinatra/reloader" unless ENV["RACK_ENV"] == "production"
 
 INTRO_SENTENCES = [
   "There's really only one solution to this:",
@@ -11,23 +12,29 @@ INTRO_SENTENCES = [
   "My time-honored approach to this problem can be found below:"
 ]
 
-def generate_locals
+def generate_locals(title = nil, seed = nil)
+  title = "#{params[:title].tr("-", " ").titleize}?" if title
+  title ||= "How Do I Solve All My Problems?"
+
+  seed ||= title.hash
+  srand seed
+  Faker::Config.random = Random.new(seed)
+
   {
+    title: title,
     intro: INTRO_SENTENCES.sample,
     author: Faker::Name.name_with_middle,
     farewell: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
     expertise: Faker::Company.catch_phrase.pluralize,
     company_name: Faker::Company.name,
-    languages: rand(3..6).times.map { |i| Faker::ProgrammingLanguage.name }.join(", ").sub(/.*\K, /, " and "),
+    languages: Array.new(rand(3..6)) { Faker::ProgrammingLanguage.name }.join(", ").sub(/.*\K, /, " and "),
     quote: Faker::Marketing.buzzwords.titleize,
     footer_type: Faker::Company.type
   }
 end
 
-get "/" do
-  erb :page, locals: generate_locals.merge(title: "How Do I Solve All My Problems?")
-end
+get "/:title?" do
+  seed = Time.now.to_i if params[:random].present?
 
-get "/:title" do
-  erb :page, locals: generate_locals.merge(title: params[:title].titleize.gsub("-", " "))
+  erb :page, locals: generate_locals(params[:title], seed)
 end
